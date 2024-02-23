59ca2a5b451d58b122dde5bfa2afed989b2b3f99
LEEjunswer <126369781+LEEjunswer@users.noreply.github.com>
2024-02-23 11:50:43 +0900

    Update Controller.java

--- TeamProject1/src/main/java/frontcontorller/Controller.java
+++ TeamProject1/src/main/java/frontcontorller/Controller.java
@@ -7,5 +7,5 @@ import jakarta.servlet.http.HttpServletRequest;
 
 public interface Controller {
 	public String requestHandler(HttpServletRequest req,HttpServletRequest resopnse) throws ServletException, IOException;
-	
+	//ㅗㅓㅎ
 }
